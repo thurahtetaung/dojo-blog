778a7bd77b7d7c24d8cd03ca21fcf48eaccf0c3a
Thura Htet Aung <thurahtetaung711@gmail.com>
2022-06-19 22:59:35 +0630

    Finish

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Dojo Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create" style={{ 
-          color: 'white', 
+        <Link to="/">Home</Link>
+        <Link to="/create" style={ {
+          color: 'white',
           backgroundColor: '#f1356d',
-          borderRadius: '8px' 
-        }}>New Blog</a>
+          borderRadius: '8px'
+        } }>New Blog</Link>
       </div>
     </nav>
   );
-}
- 
+};
+
 export default Navbar;
--- src/useFetch.js
+++ src/useFetch.js
@@ -4,9 +4,13 @@ const useFetch = (url) => {
   const [ data, setData ] = useState(null);
   const [ isPending, setIsPending ] = useState(true);
   const [ error, setError ] = useState(null);
+
+
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then(
           res => {
             if (!res.ok) {
@@ -20,10 +24,17 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch(err => {
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          }
+          else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
-    }, 1000);
+    }, 400);
+
+    return () => abortCont.abort();
   }, [ url ]);
 
   return { data, isPending, error };
